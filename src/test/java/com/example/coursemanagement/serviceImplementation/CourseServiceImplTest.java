5d461b57d437f7c69d7e1ef68c2a2cb98d926ee7
Adham Shaheen <adhamshaheen282@gmail.com>
2026-07-09 22:20:29 +0300

    Remove a non used import line in CourseServiceImplTest.java

--- src/test/java/com/example/coursemanagement/serviceImplementation/CourseServiceImplTest.java
+++ src/test/java/com/example/coursemanagement/serviceImplementation/CourseServiceImplTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
 import java.util.ArrayList;
 import java.util.List;
